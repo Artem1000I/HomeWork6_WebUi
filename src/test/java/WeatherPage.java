7b8f946bbdc321609b84594e86a6dacf324cb5ab
filol <filolog999333@mail.ru>
2022-12-04 21:01:37 +1000

    HomeWork_6

--- src/test/java/WeatherPage.java
+++ src/test/java/WeatherPage.java
@@ -1,2 +1,101 @@
-package PACKAGE_NAME;public class WeatherPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WeatherPage {
+    private WebDriver driver;
+
+    private final String button_login = "Кнопка войти";
+    private final String button_mail = "Поле ввода эмейл";
+
+    public WeatherPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void mapWeather() {
+        driver.findElement(By.xpath(".//span[text()='Карта осадков']")).click();
+    }
+
+    //YandexTest
+    public void buttonLogin() {
+        driver.findElement(By.xpath("//span[text()='Войти']")).click();
+    }
+
+    public void inputEmail() {
+        driver.findElement(By.id("passp-field-login")).click();
+    }
+
+    public void inputEmailSend() {
+        driver.findElement(By.id("passp-field-login")).sendKeys("AnTONTON111");
+    }
+
+    public void signIn() {
+        driver.findElement(By.id("passp:sign-in")).click();
+    }
+
+    public void inputPassword() {
+        driver.findElement(By.id("passp-field-passwd")).click();
+    }
+
+    public void failedPassword() {
+        driver.findElement(By.id("passp-field-passwd")).sendKeys("Anton111anto");
+    }
+
+    public void password() {
+        driver.findElement(By.id("passp-field-passwd")).sendKeys("Anton111anton");
+    }
+
+    //test 3
+    public void showOnMap() {
+        driver.findElement(By.xpath(".//div[text()='Показать на карте']")).click();
+    }
+
+    //Test 4
+    public void rainfall() {
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/span/label[1]")).click();
+    }
+
+    public void nowcast() {
+        driver.findElement(By.xpath("/html/body/div[3]/div[2]/span/label[2]")).click();
+    }
+
+    public void temperature() {
+        driver.findElement(By.xpath(".//input[@value='temperature']")).click();
+    }
+
+    public void wind() {
+        driver.findElement(By.xpath(".//input[@value='wind']")).click();
+    }
+
+    public void pressure() {
+        driver.findElement(By.xpath(".//input[@value='pressure']")).click();
+    }
+
+    //test5
+    public void yandex() {
+        driver.findElement(By.xpath(".//span[text()='Прогноз на 10 дней']")).click();
+    }
+
+    public void tenDay() {
+        driver.findElement(By.xpath(".//a[text()='Подробный прогноз на 10 дней']")).click();
+    }
+
+    //Test6
+    public void search() {
+        driver.findElement(By.cssSelector(".mini-suggest__input")).click();
+    }
+
+    public void searchSendKeys() {
+        driver.findElement(By.cssSelector(".mini-suggest__input")).sendKeys("Мурманск");
+    }
+
+    public void enter() {
+        driver.findElement(By.cssSelector(".mini-suggest__input")).sendKeys(Keys.ENTER);
+    }
+
+    //Test7
+    public void month() {
+        driver.findElement(By.xpath(".//a[text()='На месяц']")).click();
+    }
 }
--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,2 +1,128 @@
-package PACKAGE_NAME;public class YandexTest {
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YandexTest extends AbstractTest {
+    @Order(1)
+    @Test
+    void myActiontest() throws InterruptedException {
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10)).until(ExpectedConditions.urlContains("https://yandex.ru/pogoda/?lat=43.785049&lon=131.971069&win=564"));
+        String URL = getDriver().getCurrentUrl(); //Проверяем урл главной страницы
+        Assertions.assertEquals(URL, "https://yandex.ru/pogoda/?lat=43.785049&lon=131.971069&win=564" );
+    }
+    @Order(2)
+    @Test
+    void inputTest() {
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.buttonLogin();
+        weatherPage.inputEmail();
+        weatherPage.inputEmailSend();
+        weatherPage.signIn();
+        weatherPage.failedPassword();
+        weatherPage.signIn();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement errorMessage = getDriver().findElement(By.id("field:input-passwd:hint"));
+        Assertions.assertEquals(errorMessage.getText(),"Неверный пароль");
+        weatherPage.inputPassword();
+        weatherPage.password();
+        weatherPage.signIn();
+    }
+    @Order(3)
+    @Test
+    void mapTest() {
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.showOnMap();
+
+        Assertions.assertNotNull(getDriver().findElement(By.cssSelector(".b-page button")).isDisplayed());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Order(4)
+    @Test
+    void
+    sectionsMapTest(){
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.showOnMap();
+        weatherPage.rainfall();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//span[text()='Молнии']")).isDisplayed());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        weatherPage.nowcast();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//div[text()='см']")).isDisplayed());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        weatherPage.temperature();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//div[text()='°C']")).isDisplayed());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        weatherPage.wind();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//div[text()='м/с']")).isDisplayed());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        weatherPage.pressure();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//div[text()='мм рт. ст.']")).isDisplayed());
+
+    }
+    @Order(5)
+    @Test
+    void forecastTenDayTest() {
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.yandex();
+        //Assert проверяем что находимся на главной
+        String URL = getDriver().getCurrentUrl(); //Проверяем урл главной страницы //для данного пользователя
+        Assertions.assertEquals(URL, "https://yandex.ru/pogoda/?lat=43.785049&lon=131.971069&via=hnav" );
+        weatherPage.tenDay();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//p[text()='Прогноз погоды в Уссурийске на сегодня, завтра и на ближайшую неделю с точностью до района — рассчитан с помощью ']")).isDisplayed());
+        getDriver().navigate().back();
+    }
+
+    @Order(6)
+    @Test void searchCityTest(){
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.search();
+        weatherPage.searchSendKeys();
+        weatherPage.enter();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//h1[text()='Вы искали: Мурманск']")).isDisplayed());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Order(7)
+    @Test
+    void weatherMonthTest() {
+        WeatherPage weatherPage =new WeatherPage(getDriver());
+        weatherPage.month();
+        Assertions.assertNotNull(getDriver().findElement(By.xpath(".//span[text()='Сравнить']")).isDisplayed());
+    }
+
+
 }
